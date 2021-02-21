fa54db1e900d77d50fbfbc2e3a48c4d6a9dbeae5
studiocliq <parkgone23@gmail.com>
2021-02-22 01:53:59 +0900

    #2.0 Creating your first React Component

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Movie App
 
 React JS Fundamentals Course
+
+- 컴포넌트(component)는 HTML을 반환하는 함수다.
---
+++ src/Potato.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Potato() {
+    return <div><h3>I love Potatos</h3></div>
+}
+
+export default Potato;
