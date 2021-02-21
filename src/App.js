d1224acee89cf23473799465a14ce56113d46588
studiocliq <parkgone23@gmail.com>
2021-02-22 05:09:24 +0900

    2.2 Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
 import React from 'react';
 
-function Food({ favorite, second }) {
-  return <div>I like { favorite } and  { second }</div>
+const favoriteFoods = [
+  {
+    name: "kimchi",
+    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.EhDEoNHa6MJ9YJKE9IN7rQHaDt%26pid%3DApi&f=1"
+  },
+  {
+    name: "ramen",
+    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.V4QOhRbzCD9PNYnbjpp3mAHaEK%26pid%3DApi&f=1"
+  },
+  {
+    name: "apple",
+    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.vi98k7Pu8GemAoIRivf3yAHaDt%26pid%3DApi&f=1"
+  },
+  {
+    name: "pizza",
+    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.XF7m9Bf6YeFD7qCiUk9_lgHaEK%26pid%3DApi&f=1"
+  }
+]
+
+function Food({ foodName, picture }) {
+  return (
+  <div>
+    <h3>I like { foodName }</h3>
+    <img src = {picture} />
+  </div>
+  );
 }
 
 
 function App() {
   return (
-  <div>
-    <h1>HELLO</h1>
-
-    <Food favorite="kimchi" second = "sugar"/>
-    <Food favorite="ramen" second = "salt"/>
-    <Food favorite="apple" second = "ractose"/>
-    <Food favorite="pizza" second = "cheese"/>
-  </div>);
+    <div>
+      {favoriteFoods.map( food => (
+        <Food foodName = {food.name} picture = {food.image} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
